ae1e295c9c9887e93f4ca108611df03f92f665b1
abhaysrivastav <abhaysrivastav@users.noreply.github.com>
2017-12-03 23:09:38 +0530

    Create LED_Control_MQTT.c

---
+++ LED_Control_MQTT.c
@@ -0,0 +1,116 @@
+  // Libraries
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Credentials
+char ssid[] = "XXXX";     //  your network SSID (name)
+char pass[] = "XXXXXXX";  // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+char server[] = {"iot.eclipse.org"};
+int port = 1883;
+char topic[] = {"codifythings/testMessageD"};
+WiFiClient client;
+int ledPin = 2;
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  // Print payload
+  String payloadContent = String((char *)payload);
+  Serial.println("[INFO] Payload: " + payloadContent);
+  turnLightsOnOff();
+}
+
+PubSubClient pubSubClient(server, 1883, callback, client);
+
+void setup() {
+  
+  // Serial 
+  Serial.begin(115200);
+
+  // Attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    Serial.println(status);
+    // Wait 10 seconds for connection:
+    delay(30000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("Youre connected to the network");
+  printWifiData();
+
+  // Set LED pin mode
+    pinMode(ledPin, OUTPUT);
+    
+   //Connect MQTT Broker
+  Serial.println("[INFO] Connecting to MQTT Broker");
+  if (pubSubClient.connect("arduinoClient1"))
+  {
+    Serial.println("[INFO] Connection to MQTT Broker Successful");
+
+    pubSubClient.subscribe(topic);
+    Serial.println("[INFO] Successfully Subscribed to MQTT Topic ");
+
+    //Serial.println("[INFO] Publishing to MQTT Broker");
+    //pubSubClient.publish(topic, "Test Message");
+  }
+  else
+  {
+    Serial.println("[INFO] Connection to MQTT Broker Failed");
+  } 
+
+}
+
+void loop() {
+
+  // Wait for messages from MQTT broker
+  pubSubClient.loop();
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+}
+
+void turnLightsOnOff()
+{
+  // Check if lights are currently on or off
+  if(digitalRead(ledPin) == LOW)
+  {
+    //Turn lights on
+    Serial.println("[INFO] Turning lights on");
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    // Turn lights off
+    Serial.println("[INFO] Turning lights off");
+    digitalWrite(ledPin, LOW);
+  }
+}
